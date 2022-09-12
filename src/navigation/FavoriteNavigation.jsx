8d1ca7d990fbab46e3a13a222503f45e840b7a50
Exequiel65 <marcos97britos@gmail.com>
2022-09-12 13:02:36 -0300

    add components and configure react navigation

---
+++ src/navigation/FavoriteNavigation.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Favorite from '../screens/Favorite'
+
+const Stack = createNativeStackNavigator()
+
+export default function FavoriteNavigation() {
+  return (
+    <Stack.Navigator>
+        <Stack.Screen name="Favorite1" component={Favorite} options={{
+            title: 'Favoritos'
+        }} />
+    </Stack.Navigator>
+  )
+}
